324ed15d65a2b0b830edf9f4f0fd3bb05059541a
guireino <guiabucarma@hotmail.com>
2017-11-23 16:10:47 -0300

    aula 2 criando class modelos e utilizando properties

--- app/src/main/java/com/example/guilherme/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/com/example/guilherme/financask/ui/activity/ListaTransacoesActivity.kt
@@ -2,10 +2,13 @@ package com.example.guilherme.financask.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.ArrayAdapter
 import com.example.guilherme.financask.R
+import com.example.guilherme.financask.model.Tipo
+import com.example.guilherme.financask.model.Transacao
 import com.example.guilherme.financask.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import java.math.BigDecimal
+import java.util.*
 
 /**
  * Created by guilherme on 14/11/17.
@@ -17,11 +20,12 @@ class ListaTransacoesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
-        val lista: List<String> = listOf("Comida - R$ 20,50", "Rmonomia - R$ 100,00")
+        val lista = listOf(Transacao(valor = BigDecimal(20.5), data = Calendar.getInstance(),
+                           categoria = "almoço de final semana", tipo = Tipo.DESPESA),
+                           Transacao(valor =  BigDecimal(100.0), tipo = Tipo.RECEITA, categoria = "Economia"),
+                           Transacao(valor = BigDecimal(200.0), tipo = Tipo.RECEITA, categoria = "Economia"))
 
-        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, lista)
-
-        lista_transacoes_listview.setAdapter(ListaTransacoesAdapter(lista, this));
+        lista_transacoes_listview.adapter = ListaTransacoesAdapter(lista, this);
     }
 
 }
--- app/src/main/java/com/example/guilherme/financask/ui/adapter/ListaTransacoesAdapter.kt
+++ app/src/main/java/com/example/guilherme/financask/ui/adapter/ListaTransacoesAdapter.kt
@@ -1,14 +1,18 @@
 package com.example.guilherme.financask.ui.adapter
 
 import android.content.Context
+import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import com.example.guilherme.financask.R
+import com.example.guilherme.financask.extension.format_br
+import com.example.guilherme.financask.extension.formatadatabr
+import com.example.guilherme.financask.extension.limitaEmAte
+import com.example.guilherme.financask.model.Tipo
 import com.example.guilherme.financask.model.Transacao
 import kotlinx.android.synthetic.main.transacao_item.view.*
-import java.text.SimpleDateFormat
 
 /**
  * Created by guilherme on 15/11/17.
@@ -18,18 +22,33 @@ class ListaTransacoesAdapter(transacoes: List<Transacao>, context: Context) : Ba
     private val transacoes = transacoes
     private val context = context
 
+    private val limiteDaCategoria = 14
+
     override fun getView(posicao: Int, view: View?, parent: ViewGroup?): View {
         val viewCriada = LayoutInflater.from(context).inflate(R.layout.transacao_item, parent, false)
 
         val transacao = transacoes[posicao]
 
-        viewCriada.transacao_valor.text = transacao.valor.toString()
-        viewCriada.transacao_categoria.text = transacao.categoria
+        if (transacao.tipo == Tipo.RECEITA) {
+            viewCriada.transacao_valor.setTextColor(ContextCompat.getColor(context, R.color.receita))
+        } else {
+            viewCriada.transacao_valor.setTextColor(ContextCompat.getColor(context, R.color.despesa))
+        }
 
-        val formatobr = "dd/MM/yyyy"
-        val format = SimpleDateFormat(formatobr)
-        val dataFormatada = format.format(transacao.data.time)
-        viewCriada.transacao_data.text = dataFormatada
+        if (transacao.tipo == Tipo.RECEITA){
+            viewCriada.transacao_icone.setBackgroundResource(R.drawable.icone_transacao_item_receita)
+        }else{
+            viewCriada.transacao_icone.setBackgroundResource(R.drawable.icone_transacao_item_despesa)
+        }
+
+//        var categoriaFormatada = transacao.categoria
+//        if(categoriaFormatada.length > 14){
+//            categoriaFormatada = "${categoriaFormatada.substring(0, 14)}..."
+//        }
+
+        viewCriada.transacao_valor.text = transacao.valor.format_br()
+        viewCriada.transacao_categoria.text = transacao.categoria.limitaEmAte(limiteDaCategoria)
+        viewCriada.transacao_data.text = transacao.data.formatadatabr()
 
         return viewCriada
     }
